2f7b51900d60b13b089202cada08900a5e4a53aa
ravigill3936 <gill1234ravi@gmail.com>
2025-05-30 21:25:52 -0400

    it works

--- index.ts
+++ index.ts
@@ -11,13 +11,13 @@ dotenv.config({});
 ConnectDB();
 
 const app = express();
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     credentials: true,
-//     methods: ["GET", "POST", "PATCH", "POST"],
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "POST"],
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json({ limit: "34kb" }));
